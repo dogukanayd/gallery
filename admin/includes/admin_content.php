9b6d7e0710117438c17eab5c65d7e26c6a638514
dogukanayd <daydogdu93@gmail.com>
2017-01-24 18:53:09 +0300

    The User Class

---
+++ admin/includes/admin_content.php
@@ -0,0 +1,42 @@
+<?php include("includes/User.php") ?>
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                Admin
+                <small>Subheading</small>
+            </h1>
+
+            <?php
+
+
+          /*  $result_set = User::find_all_users();
+            while($row = mysqli_fetch_array($result_set))
+            {
+                echo $row['username'] . "<br>";
+
+            }*/
+
+            $found_user = User::find_user_by_id(2);
+            echo $found_user["username"];
+
+            //$user_found = mysqli_fetch_array($result);
+
+
+            ?>
+
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                </li>
+                <li class="active">
+                    <i class="fa fa-file"></i> Blank Page
+                </li>
+            </ol>
+        </div>
+    </div>
+    <!-- /.row -->
+
+</div>
