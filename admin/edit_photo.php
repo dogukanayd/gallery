04f705b7fd44bb2131160578d1d4243bb17e63bf
dogukanayd <daydogdu93@gmail.com>
2017-03-02 18:22:44 +0300

    Edit Page

---
+++ admin/edit_photo.php
@@ -0,0 +1,59 @@
+<?php include("includes/header.php"); ?>
+
+<?php
+if(!$session->is_signed_in())
+{
+    redirect("login.php");
+
+}else
+{
+
+}
+?>
+
+<?php
+
+//$photos = Photo::find_all();
+
+?>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+
+        <?php include("includes/top_nav.php"); ?>
+
+        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+        <?php include("includes/side_nav.php") ?>
+        <!-- /.navbar-collapse -->
+    </nav>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Photos
+                        <small>Subheading</small>
+                    </h1>
+                    <div class="col-md-12">
+
+                        <div class="form-group">
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+<?php include("includes/footer.php"); ?>
