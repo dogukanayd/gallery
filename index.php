5554e658855d7256a2673df8f705cc757d46b187
dogukanayd <daydogdu93@gmail.com>
2017-03-23 02:39:38 +0300

    Some Front End Touches

--- index.php
+++ index.php
@@ -1,30 +1,39 @@
 <?php include("includes/header.php"); ?>
 
-
-        <div class="row">
-
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
-
-    
-            
-          
-         
-
-            </div>
+<?php
+$photos = Photo::find_all();
+?>
 
 
+<div class="row">
+
+    <!-- Blog Entries Column -->
+    <div class="col-md-12">
+        <div class="thumbnails row">
+            <?php foreach ($photos as $photo): ?>
+
+                <div class="col-xs-6 col-md-3">
+                    <a class="thumbnail" href="photo.php?id=<?php echo $photo->id ?>">
+                        <img class="home_page_photo img-responsive" src="admin/<?php echo $photo->picture_path(); ?>" alt="">
+                    </a>
+                </div>
 
 
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
-
+                <?php endforeach; ?>
 
 
         </div>
+
+
+        <!-- Blog Sidebar Widgets Column -->
+        <!--    <div class="col-md-4">-->
+        <!---->
+        <!---->
+        <!--        --><?php //include("includes/sidebar.php"); ?>
+        <!---->
+        <!---->
+    </div>
+</div>
         <!-- /.row -->
 
         <?php include("includes/footer.php"); ?>
