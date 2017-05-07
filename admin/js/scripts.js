e5f969ba20b6313550c48e65519f7c78285a5cfa
dogukanayd <daydogdu93@gmail.com>
2017-05-07 14:31:46 +0300

    Information about the photo while changing the user's photo

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
 	var image_href_splitted;
 	var image_name;
 
+	var photo_id;
+
 	$('.modal_thumbnails').click(function(){
 		$('#set_user_image').prop('disabled', false);
 
@@ -17,6 +19,20 @@ $(document).ready(function(){
 		image_src = $(this).prop('src');
 		image_href_splitted = image_src.split('/');
 		image_name = image_href_splitted[image_href_splitted.length -1];
+
+		photo_id = $(this).attr('data');
+
+		$.ajax({
+			url: "includes/ajax_code.php",
+			data: {photo_id: photo_id},
+			type: "POST",
+			success: function(data){
+				if(!data.error){
+					$("#modal_sidebar").html(data);	
+				}
+			}
+		});
+
 	});
 
 	$('#set_user_image').click(function(){
